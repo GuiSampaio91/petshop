e38bd2e07c09a4f2d82efe492edc21eafef8fafd
GuiSampaio91 <143126300+GuiSampaio91@users.noreply.github.com>
2024-09-09 14:55:41 -0300

    Delete petshop-site directory

--- petshop-site/js/script.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Scripts para inicializar o carrossel e outras funcionalidades
-    console.log("Página carregada com sucesso.");
-});
